f5f451167050a9fd2c188b6035f08bb1255b4fd1
Lucas Moreira <lucamoreira256@gmail.com>
2022-10-18 12:41:25 -0300

    Criado formulário de projeto

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -6,6 +6,22 @@
     <button class="button" @click="alterTheme">
       {{ buttonThemeText }} modo escuro
     </button>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            Tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projects" class="link">
+            <i class="fas fa-project-diagram"></i>
+            Projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -50,5 +66,17 @@ header {
     padding: 2.5rem;
     height: auto;
   }
+}
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #FAF0CA;
+}
+.link.router-link-active {
+  color: #FAF0CA;
 }
 </style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="projects">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="save">
+      <div class="field">
+        <label for="projectName" class="label">
+          Nome do Projeto
+        </label>
+        <input type="text" class="input" v-model="projectName" id="projectName">
+      </div>
+      <div class="field">
+        <button class="button" type="submit">
+          Salvar
+        </button>
+      </div>
+    </form>
+    <table class="table is-fullwidth">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>Nome</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="project in projects" :key="project.id">
+        <td>{{ project.id }}</td>
+        <td>{{ project.name }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script lang="ts">
+import IProject from "@/interfaces/IProject";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'ProjectsView',
+  data() {
+    return {
+      projectName: '',
+      projects: [] as IProject[]
+    }
+  },
+  methods: {
+    save() {
+      const project: IProject = {
+        name: this.projectName,
+        id: new Date().toISOString()
+      }
+      this.projects.push(project)
+      this.projectName = ''
+    }
+  }
+})
+</script>
+
+<style scoped>
+.projects {
+  padding: 1.25rem;
+}
+</style>
